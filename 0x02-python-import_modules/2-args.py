a5deb69da1f69641ba9f3f788e20b3cdd4b1eae3
Veronicandemo <ndemoveronica@gmail.com>
2024-04-07 20:14:00 +0300

    calculator

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ != "__main__":
     exit()
-
 argStr = "{:d} argument"
 argc = len(sys.argv) - 1
 if argc == 0:
@@ -13,7 +11,6 @@ elif argc == 1:
 else:
     argStr += 's:'
 print(argStr.format(argc))
-
 i = 0
 for arg in sys.argv:
     if i != 0:
