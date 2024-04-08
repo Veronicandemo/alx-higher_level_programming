92dbc9aa54cdc35e688ae6d7142be597df0881f7
Veronicandemo <ndemoveronica@gmail.com>
2024-04-08 13:57:23 +0300

    computes the square value of all integers of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,4 +5,3 @@ def square_matrix_simple(matrix=[]):
         for idx2 , col in enumerate(new_matrix):
             new_matrix[idx][idx2] = row[idx2] ** 2
     return new_matrix
-            
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,1 +1,3 @@
 # 0x04. Python - More Data Structures: Set, Dictionary
+
+This directory includes all the solutions to More Data Structures: Set, Dictionary Python
