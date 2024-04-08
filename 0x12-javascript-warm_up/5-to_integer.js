70bbfe4a216efd89e0190cec2f5c7e3409669412
Veronicandemo <ndemoveronica@gmail.com>
2024-04-08 23:27:14 +0300

    first argument can be converted to an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+const [, , arg] = process.argv;
+
+const parsedNumber = parseInt(arg);
+
+if (!isNaN(parsedNumber)) {
+  console.log(`My number: ${parsedNumber}`);
+} else {
+  console.log("Not a number");
+}
