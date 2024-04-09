5394e46648efb14fed4622c5a2608204d66efff3
Veronicandemo <ndemoveronica@gmail.com>
2024-04-09 18:37:52 +0300

    Defines a rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-module.export = class Rectangle {
+module.exports = class Rectangle {
   constructor(w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print() {
     for (let i = 0; i < this.height; i++) {
       console.log("X".repeat(this.width));
